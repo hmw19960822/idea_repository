af9168874d4e5790fc9991bb9bff73dee83cab9e
LJJ <511709197@qq.com>
2020-04-11 12:59:47 +0800

    这是合并之后的结果并推送

--- product_service/src/main/java/cn/itcast/product/controller/TestController.java
+++ product_service/src/main/java/cn/itcast/product/controller/TestController.java
@@ -6,6 +6,8 @@ package cn.itcast.product.controller;
  */
 public class TestController {
     public static void main(String[] args) {
+        System.out.println("这是右侧的打印测试");
         System.out.println("这是左侧的打印测试");
     }
+
 }

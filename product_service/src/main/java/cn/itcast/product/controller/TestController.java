ce6fe07d09f4beafb3a5b4aac31cac0d19fdb717
LJJ <511709197@qq.com>
2020-04-11 12:54:50 +0800

    这是左侧TestController推送的打印测试

--- product_service/src/main/java/cn/itcast/product/controller/TestController.java
+++ product_service/src/main/java/cn/itcast/product/controller/TestController.java
@@ -5,4 +5,7 @@ package cn.itcast.product.controller;
  * @create 2020-04-11 9:24
  */
 public class TestController {
+    public static void main(String[] args) {
+        System.out.println("这是左侧的打印测试");
+    }
 }
